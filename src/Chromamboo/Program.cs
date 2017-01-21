2e3b7c15a7683eac5d399463931ed74bec9dbda2
Fabio Salvalai <fabio@seesharp.ch>
2017-01-21 11:08:28 +0100

    cleanup

--- src/Chromamboo/Program.cs
+++ src/Chromamboo/Program.cs
@@ -1,31 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 using Chromamboo.Contracts;
-using Chromamboo.Providers.Presentation;
-using Chromamboo.Providers.Presentation.Contracts;
+
 using Ninject;
 using Ninject.Extensions.Conventions;
 
-using System.Linq;
-
 namespace Chromamboo
 {
-
-
     public class Program
     {
-        private static IBambooApi bambooApi;
-
-        private static IBitbucketApi bitbucketApi;
-
-       // private static IPresentationService presentationService;
-
         private static void Main(string[] args)
         {
             var kernel = BuildKernel();
             var bootstrapper = kernel.Get<IBoostrapper>();
-            bootstrapper.Start();            
-            // TODO: get a push notification from the bamboo server whenever a new build is in.
+            bootstrapper.Start();        
+                
             Console.WriteLine("Hit any key to exit...");
             Console.ReadKey();
         }
@@ -33,7 +23,6 @@ namespace Chromamboo
         private static IKernel BuildKernel()
         {
             var kernel = new StandardKernel();
-
 
             kernel.Bind(x =>
                 x.FromThisAssembly()
@@ -43,18 +32,6 @@ namespace Chromamboo
                     .BindSelection((type, types) => type.GetInterfaces().Take(1)));
 
             return kernel;
-        }
-
-        private static IGitNotificationPresentationProvider[] GetGitNotificationProviders(string[] gitNotificationPresentationProviderNames)
-        {
-            // TODO: don't hardcode it.
-            return new IGitNotificationPresentationProvider[]
-                    {
-                        new RazerChromaGitNotificationPresentationProvider(),
-                        new BlyncGitNotificationPresentationProvider()
-                    }
-                    .Where(provider => gitNotificationPresentationProviderNames.Any(n => n.Equals(provider.Name, StringComparison.OrdinalIgnoreCase)))
-                    .ToArray();
         }
     }
 }
