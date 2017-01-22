794a804083c3f3bb26d6e57a77f1ca09bfe0cf79
Fabio Salvalai <fabio@seesharp.ch>
2017-01-22 22:11:26 +0100

    List all settings parameters with react components.
    yes, it's ugly!

--- src/Chromamboo/wwwroot/chromamboo-react/src/App.js
+++ src/Chromamboo/wwwroot/chromamboo-react/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import createFragment from 'react-addons-create-fragment'
 
 class App extends Component {
-  render() {
+
+  constructor() {
+      super();
+      this.state = {notifications: []}
+  }
+  componentWillMount(){
+      fetch ('http://localhost:5000/config')
+      .then( response => response.json())
+      .then( ({notifications: notifications})=> this.setState({notifications}) )
+  }
+  render(){
+    let items = this.state.notifications;
+    console.log(items);
     return (
       <div className="App">
         <div className="App-header">
@@ -11,11 +24,64 @@ class App extends Component {
           <h2>Chromamboo configuration</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. yay!
+            Current configuration
         </p>
+        <div>
+
+            {items.map(item =>
+                <NotificationBlock key={item.displayName} settings={item} />
+            )}
+        </div>
       </div>
     );
   }
+}
+
+
+
+class NotificationBlock extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        let settings = this.props.settings;
+        let parameters = createFragment({
+            right: Object.keys(settings),
+            left: Object.keys(settings)
+        });
+        return (
+            <div>
+                <h4>
+                    {settings.displayName}
+                </h4>
+                <ul>
+
+
+                    {Object.keys(settings).map(function(keyName, keyIndex) {
+                        console.log(settings[keyName]);
+                        // use keyName to get current key's name
+                        // and a[keyName] or a.keyName to get its value
+                        return <li key={keyName}>Key: {keyName} <ValueEditor content={settings[keyName]} /></li>
+                    })}
+                </ul>
+            </div>)
+    }
+}
+
+class ValueEditor extends Component {
+    constructor() {
+        super();
+    }
+    render() {
+        let content = this.props.content;
+        if (typeof content == "object"){
+            return <NotificationBlock settings={content}/>
+        }
+        return(
+        <div>
+            {this.props.content}
+        </div>)
+    }
 }
 
 export default App;
