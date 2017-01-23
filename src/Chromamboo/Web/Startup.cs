6acc118eaaf976bc8f6fe3a2f2f9a9c697c218b7
salfab <fabio@seesharp.ch>
2017-01-23 19:09:03 +0100

    try to serve the react app with reactjs.net

--- src/Chromamboo/Web/Startup.cs
+++ src/Chromamboo/Web/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using React.AspNet;
 
 namespace Chromamboo.Web
 {
@@ -7,15 +9,36 @@ namespace Chromamboo.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
-          services
-                .AddMvcCore()
+            services
+                .AddSingleton<IHttpContextAccessor, HttpContextAccessor>()
+                .AddReact()
+                .AddMvcCore()                
                 .AddJsonFormatters();
 
         }
         public void Configure(IApplicationBuilder app)
         {
-            app.UseStaticFiles()
-                .UseMvc();
+            app
+                .UseStaticFiles()
+                .UseMvc()
+                .UseReact(config =>
+                {
+                    // If you want to use server-side rendering of React components,
+                    // add all the necessary JavaScript files here. This includes
+                    // your components as well as all of their dependencies.
+                    // See http://reactjs.net/ for more information. Example:
+                    //config
+                    //  .AddScript("~/Scripts/First.jsx")
+                    //  .AddScript("~/Scripts/Second.jsx");
+
+                    // If you use an external build too (for example, Babel, Webpack,
+                    // Browserify or Gulp), you can improve performance by disabling
+                    // ReactJS.NET's version of Babel and loading the pre-transpiled
+                    // scripts. Example:
+                    //config
+                    //  .SetLoadBabel(false)
+                    //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+                });
         }
     }
 }
