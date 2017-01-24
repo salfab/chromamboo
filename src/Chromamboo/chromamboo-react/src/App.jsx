25aaba71d3a7486cb99920946ce4473c2f1f924a
salfab <fabio@seesharp.ch>
2017-01-24 19:37:30 +0100

    transpile the JSX. (the copy task is still a bit dodgy.

--- src/Chromamboo/chromamboo-react/src/App.jsx
+++ src/Chromamboo/chromamboo-react/src/App.jsx
@@ -21,7 +21,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Chromamboo configuration</h2>
+          <h2>Chromamboo configuration!</h2>
         </div>
         <p className="App-intro">
             Current configuration
