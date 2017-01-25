208872af82cbb86461510c5978278e6275ee205f
Fabio Salvalai <fabio@seesharp.ch>
2017-01-25 20:37:42 +0100

    Adding way too much logic in a .cmd file.
    being old-school, juste like that :P

--- src/publish-website.cmd
+++ src/publish-website.cmd
@@ -1,5 +1,19 @@
+@echo off
+
+IF [%1]==[] goto undefined
+SET buildconfig=%1
+
+goto start
+:undefined
+SET buildconfig=debug
+echo.
+echo no build configuration specified, using 'debug' as a default.
+:start
+echo Preparing chromamboo-react for %buildconfig%
 cd Chromamboo
 cd chromamboo-react
 Call npm run build
-echo deploying files to wwwroot
-xcopy build ..\wwwroot /s /e /h /I
+echo deploying files to the '%buildconfig%' binaries
+xcopy build ..\bin\%buildconfig%\wwwroot  /s /e /h /I /Y
+cd..
+cd..
